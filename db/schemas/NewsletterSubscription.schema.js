0e0d093099f7a95d382b3e2f14e3191bdcaf30c3
George Benjamin <msdcconnect@gmail.com>
2019-12-28 12:45:57 +0100

    fix newsletter subscription issue

--- db/schemas/NewsletterSubscription.schema.js
+++ db/schemas/NewsletterSubscription.schema.js
@@ -1,30 +1,38 @@
 const mongoose = require('mongoose');
 
+const locationSchema = require('./Location.Schema');
+
 const { Schema } = mongoose;
 
-// TODO: I need a way to standardize locations
 
-const newsletterSubscriptionSchema = new Schema({
-  email: {
-    type: String,
-    lowercase: true,
-    trim: true,
-    required: true,
-    unique: true,
+const newsletterSubscriptionSchema = new Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    frequency: {
+      type: String,
+      enum: ['DAILY', 'WEEKLY'],
+      required: true,
+    },
+    address: {
+      location: locationSchema,
+      formatted_address: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
   },
-  frequency: {
-    type: String,
-    enum: ['DAILY', 'WEEKLY'],
-    required: true,
-  },
-  state: {
-    type: String,
-    default: 'all',
-  },
-  country: {
-    type: String,
-    default: 'all',
-  },
-}, { timestamps: true });
+  { timestamps: true },
+);
 
 module.exports = newsletterSubscriptionSchema;
