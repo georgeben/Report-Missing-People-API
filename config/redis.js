22e0bc2d05a2023174341fc0b5c3376be076cfe0
George Benjamin <msdcconnect@gmail.com>
2020-01-07 15:46:58 +0100

    fix redis client connection

--- config/redis.js
+++ config/redis.js
@@ -4,14 +4,22 @@
 
 const redis = require('redis');
 const bluebird = require('bluebird');
+const url = require('url');
 const { logger } = require('../utils');
 // const { redisUrl } = require('../config');
 require('dotenv').config();
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
-
-console.log('Redis url', process.env.REDIS_URL);
-const client = redis.createClient(process.env.REDIS_URL);
+let client;
+if (process.env.REDIS_URL) {
+  const redisUrl = url.parse(process.env.REDIS_URL);
+  console.log('Redis port', redisUrl.port);
+  console.log('Redis hostname', redisUrl.hostname);
+  client = redisUrl.createClient(redisUrl.port, redisUrl.hostname);
+  client.auth(redisUrl.auth.split(':')[1]);
+} else {
+  client = redis.createClient();
+}
 
 
 client.on('connect', () => logger.log('info', 'Successfully connected to redis'));
