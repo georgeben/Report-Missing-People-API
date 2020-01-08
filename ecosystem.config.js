9d50944bca78c94c2f49ddbe6346498c039eb7f5
George Benjamin <msdcconnect@gmail.com>
2020-01-08 01:02:12 +0100

    reduce worker instance to 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       name: 'worker',
       script: 'worker.js',
-      instances: 2,
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
