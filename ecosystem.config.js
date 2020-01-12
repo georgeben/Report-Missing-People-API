a9e511f3d359d5a77d25f75d63a7c09e9b1c2dae
George Benjamin <msdcconnect@gmail.com>
2020-01-13 00:18:59 +0100

    change web instance to 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'server',
       script: 'server.js',
-      instances: 1,
+      instances: 2,
       exec_mode: 'cluster',
       autorestart: true,
       watch: false,
